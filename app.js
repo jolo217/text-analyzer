dfe5ae4f14babae29d1c5cc7b0f44b969cd7e24b
jolo217 <joloxj@yahoo.com>
2017-06-22 14:51:57 -0500

    finished

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// your code here!
+function averageWordLength(value) {
+	var wordsArray = value.split(" ");
+	var totalLength = wordsArray.join("").length;
+	return (totalLength / wordsArray.length).toFixed(2);
+}
+
+function uniqueWords(value) {
+	var wordsArray = value.split(" ");
+	var uniqueWords = [];
+	for (var i = 0; i < wordsArray.length; i++) {
+		if (uniqueWords.indexOf(wordsArray[i]) === -1) {
+			uniqueWords.push(wordsArray[i]);
+		}
+	}
+	return uniqueWords.length;
+}
+
+function WordCount(str) { 
+  return str.split(" ").length;
+}
+
+$(".submit-button").click(function(event) { 
+	event.preventDefault();
+	var writtenValue = $("#user-text").val();
+	var averageWord = averageWordLength(writtenValue);
+	var uniqueWordCount = uniqueWords(writtenValue);
+	var totalWordCount = WordCount(writtenValue);
+	$(".js-word-count").text(totalWordCount);
+	$(".js-unique-word-count").text(uniqueWordCount);
+	$(".js-average-word-length").text(averageWord);
+	$(".text-report").removeClass("hidden");
+});
